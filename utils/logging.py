6bcbb2fcd75921fb4a0c8d9de05a8e38e7923f46
ojkafka <ok2181@nyu.edu>
2024-04-02 18:42:17 -0400

    adding logging as a first pass, I would like to include naming as arguments for the logging so we automatically pass in the name of the experiment

--- utils/logging.py
+++ utils/logging.py
@@ -1,1 +1,21 @@
-def log_to_wandb():
+import wandb
+import time
+
+def log_to_wandb(results, metrics):
+    # Configure experiment
+    experiment_name = "we should change this, probably being passed in"
+    quantization_strategy = "we should change this, probably being passed in"
+    config = {
+        "experiment_name": experiment_name,
+        "quantization_type": quantization_strategy,
+    }
+
+    # Initialize experiment
+    wandb.init(project="ML_Final_Project_ODDJ", name=experiment_name, config=config)
+
+    # Log results and additional metrics
+    wandb.log(metrics)
+    wandb.log({"runtime_seconds": metrics.get('runtime', 0)})
+
+    # Finish the run
+    wandb.finish()
